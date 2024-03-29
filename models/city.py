d48b6e4db9611376d7a6f3054244bfbff3e37599
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-29 14:46:48 -0400

    updating city file

--- models/city.py
+++ models/city.py
@@ -5,12 +5,25 @@ and a class attribute name that represents the city
  name and state_id with  a foreign key to states.id
 of the State class with a string (60) """
 
+from models.base_model import BaseModel
+from models.base_model import Base
+from os import getenv
 from sqlalchemy import Column, String, ForeignKey
-from models.base_model import BaseModel, Base
 
 
-class City():
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(String(60),
+                      ForeignKey("states.id", ondelete="CASCADE"),
+                      nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        from sqlalchemy.orm import relationship
+
+        places = relationship("Place", backref="cities", cascade="all, delete")
+
+    else:
+        places = []
