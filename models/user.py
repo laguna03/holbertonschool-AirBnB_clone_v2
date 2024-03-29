7c1b6a4748fa886ab6db4e1079ba5207ccb26204
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-29 15:04:01 -0400

    updating user,py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from os import getenv
 from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    # file  for FileStorage or db for DBStorage
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        from sqlalchemy import Column, String
+        from sqlalchemy.orm import relationship
+        places = relationship("Place", backref="user", cascade="all, delete")
+        reviews = relationship("Review", backref="user", cascade="all, delete")
+
+    else:
+        places = []
+        reviews = []
