75cefd9229c0734b7e947e4ff2b5ebc8a7368b63
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-29 14:38:15 -0400

    Adding module to initiate obj class of db storage

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from models.engine.file_storage import FileStorage
 import os
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    """This module instantiates an object of class DBStorage"""
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
