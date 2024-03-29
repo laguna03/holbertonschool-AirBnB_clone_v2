b62d61b053c368ca5b6b2f69fc0738ff3d4300ea
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-03-29 14:48:41 -0400

    Updating state file

--- models/state.py
+++ models/state.py
@@ -3,10 +3,32 @@
 with a class State that inherits from BaseModel
 and a class attribute name that represents the
  state name with a string (128)"""
+from models.base_model import BaseModel
+from models.base_model import Base
+from os import getenv
 from sqlalchemy import Column, String
-from models.base_model import BaseModel, Base
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """ State class """
+
+    # file ? for FileStorage or db for DBStorage
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        from sqlalchemy.orm import relationship
+        cities = relationship("City", backref="state", cascade="all, delete")
+
+    else:
+
+        @property
+        def cities(self):
+            from models.__init__ import storage
+            from models.city import City
+
+            cities_list = []
+            for key, val in storage.all(City).items():
+                if val.state_id == self.id:
+                    cities_list.append(val)
+            return cities_list
