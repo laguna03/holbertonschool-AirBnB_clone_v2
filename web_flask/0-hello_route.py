5caccc4c8310a6c288ef7757d678d70d68eb5294
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-10 16:51:43 -0400

    first commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,18 @@
  #!/usr/bin/python3
+"""hello_route: First app for web python-flask"""
 
 from flask import Flask
 
-'''Starts a Flask web application'''
+"""Starts a Flask web application"""
 
 app = Flask(__name__)
 
-'''Defines the route and returns "Hello HBNB!"'''
+"""Defines the route and returns 'Hello HBNB!'"""
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    '''Displays Hello HBNB!'''
+    """Displays Hello HBNB!"""
     return 'Hello HBNB!'
-
-'''Defines the route '/hbnb' and returns "HBNB"'''
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
