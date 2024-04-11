6f48096ef6b93c0d2c928f8123396429d2d6085d
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-11 13:27:00 -0400

    Making changes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-""" C_route.py Task 2 """
+""" Script to start a Flask web app """
 
 from flask import Flask
+from urllib.parse import unquote_plus
 
+""" Create new Flask instance"""
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
-def index():
-    """Say Hello HBNB!"""
-    return "Hello HBNB!"
+""" Define principal route ("/") with msg "Hello HBNB!" """
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Say HBNB"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Return URL Route with variable"""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+@app.route('/c/<text>', strict_slashes=False)
+def c_display(text):
+    text = unquote_plus(text.replace('_', ' '))
+    return (f"C {text}")
+
 
 if __name__ == '__main__':
+    """
+    Configure app to listen in  0.0.0.0 in port 5000
+    """
     app.run(host='0.0.0.0', port=5000)
