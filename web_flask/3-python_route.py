d5a968ef884744b8e2605d347f25992ef897fed6
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-11 14:17:28 -0400

    Fixing 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -11,32 +11,28 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
-
-""" Define route ("/hbnb") with msg "HBNB" """
+    return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
-
-""" Define route ("/c/<text>") with msg "C <text>" """
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_display(text):
     tetx = unquote_plus(text.replace('_', ' '))
     return (f"C {text}")
 
-""" Define route ("/python/") with msg "Python is cool" """
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_display(text):
-    text = unquote_plus(text.replace('_', ' '))
-    return (f"Python {text}")
+        text = unquote_plus(text.replace('_', ' '))
+        return (f"Python {text}")
 
 
-""" Configure app to listen in 0.0.0.0 in port 5000 """
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    """ Configure app to listen in 0.0.0.0 in port 5000 """
+
+app.run(host='0.0.0.0', port=5000)
 
