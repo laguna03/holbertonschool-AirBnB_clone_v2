6b4fa114fb680b66bfeaabd05865f915d221e7c1
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-11 13:22:53 -0400

    Making file exec

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
-"""hbnb_route: First app for web python-flask"""
+""" Script to start a Flask web app """
 
 from flask import Flask
+
+""" Creacion de instancia de la aplicación Flask """
 app = Flask(__name__)
 
+""" Definir la ruta principal ("/") con el mensaje "Hello HBNB!" """
+
+
 @app.route('/', strict_slashes=False)
-def index():
-    """Say Hello"""
-    return "Hello HBNB!"
+def hello_hbnb():
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Say HBNB"""
     return "HBNB"
 
+
 if __name__ == '__main__':
+    """
+    Configurar la aplicación para que escuche en 0.0.0.0 en el puerto 5000
+    """
     app.run(host='0.0.0.0', port=5000)
